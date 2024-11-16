4f5b3c5f8f002500d5eaec66c98688dcbb05f7db
legozx777 <mikezx777@gmail.com>
2024-11-15 20:39:09 -0500

    Initial commit

---
+++ fruit.gd
@@ -0,0 +1,93 @@
+extends RigidBody2D
+
+signal seeen(index: int)
+signal score(score: int)
+signal spawn_new(index: int, pos: Vector2, poof: bool)
+signal hit
+const SCALES: Array = [
+	1, 
+	1.25, 
+	1.5, 
+	(67.0/32), # 210%, 2.09375
+	2.75, 
+	(105.0/32), # 330%, 3.29125
+	(121.0/32), # 380%, 3.78125
+	4.75, 
+	(169.0/32), # 530%, 5.28125
+	(217.0/32), # 680%, 6.78125
+	8,
+]
+const SCORES: Array = [
+	1,
+	3,
+	6,
+	10,
+	15,
+	21,
+	28,
+	36,
+	45,
+	55,
+	66,
+]
+var hitt: bool = false
+var just_player_spawned: bool = true
+var index: int
+
+func _on_body_entered(body):
+	if not hitt:
+		hitt = true
+		hit.emit()
+	if body.get_groups().find("fruit") != -1 and body.index == index:
+		var pos
+		if position.y >= body.position.y: # sets y pos to the lower fruit
+			pos = Vector2(lerp(position.x, body.position.x, 0.5), position.y)
+		else:
+			pos = Vector2(lerp(position.x, body.position.x, 0.5), body.position.y)
+		body.free()
+		score.emit(SCORES[index])
+		seeen.emit(index + 1)
+		if index != 10: # index 10 means 2 suika collided
+			spawn_new.emit(index + 1, pos, true)
+		queue_free()
+		
+
+func sett(indexx: int, pos: Vector2) -> void:
+	self.index = indexx
+	self.position = pos
+	$CollisionShape2D.scale = Vector2(SCALES[indexx], SCALES[indexx])
+	if indexx == 0:
+		$Cherry.visible = true
+	elif index == 1:
+		$Cherry.visible = false
+		$Strawberry.visible = true
+	elif index == 2:
+		$Strawberry.visible = false
+		$Grape.visible = true
+	elif index == 3:
+		$Grape.visible = false
+		$Tangerine.visible = true
+	elif index == 4:
+		$Tangerine.visible = false
+		$Orange.visible = true
+	elif index == 5:
+		$Orange.visible = false
+		$Apple.visible = true
+	elif index == 6:
+		$Apple.visible = false
+		$Melon.visible = true
+	elif index == 7:
+		$Melon.visible = false
+		$Peachnya.visible = true
+	elif index == 8:
+		$Peachnya.visible = false
+		$Pineapple.visible = true
+	elif index == 9:
+		$Pineapple.visible = false
+		$Wintermelon.visible = true
+	elif index == 10:
+		$Wintermelon.visible = false
+		$SUIKA.visible = true
+	
+func get_top_pos() -> float:
+	return self.position.y - (16 * index)
---
+++ main.gd
@@ -0,0 +1,282 @@
+extends Node2D
+
+const SAVE_FILE: String = "user://data.save"
+const MIN_CLAMPS:= [ # at 415 x
+	416 + 16, 
+	416 + 20, 
+	416 + 24, 
+	416 + 33, 
+	416 + 44, 
+]
+const MAX_CLAMPS:= [ # at 895
+	894 - 16, 
+	894 - 20, 
+	894 - 24, 
+	894 - 33, 
+	894 - 44, 
+]
+const POOF_SCALES:= [
+	0.25 * 1, 
+	0.25 * 1.25, 
+	0.25 * 1.5, 
+	0.25 * (67.0/32), 
+	0.25 * 2.75, 
+	0.25 * (105.0/32), 
+	0.25 * (121.0/32), 
+	0.25 * 4.75, 
+	0.25 * (169.0/32), 
+	0.25 * (217.0/32), 
+	0.25 * 8,
+]
+
+var master_bus = AudioServer.get_bus_index("Master")
+# vv  game data
+var playing: bool
+var game_seen: int # resets on game, which fruits can spawn
+var curr: int # fruit being dropped / held by player
+var next: int # fruit that is next
+var score: int
+
+# vv  loaded from disk
+var seen: int # wheel of evo  0-10
+var high_score: int # 0-4 billion
+var playtime: int # in seconds  0-4 billion or 136 years
+""" more shit to add
+background of falling fruits that align with evo wheel
+can transfer save data using a hash of save.data with something added infront to prevent tampering
+ ^^ (FileAccess.open_encrypted or .open_encrypted_with_pass)
+
+global leaderboad w/ ip or smth as a unique identifier for each ("cosmetic" username and highscore)
+menu screen (or not)
+death sound effect
+"""
+func _ready():
+	if FileAccess.file_exists(SAVE_FILE):
+		var file = FileAccess.open(SAVE_FILE, FileAccess.READ)
+		seen = file.get_8()
+		high_score = file.get_32()
+		playtime = file.get_32()
+		file.close()
+	else:
+		seen = 0
+		high_score = 0
+		playtime = 0
+		if FileAccess.file_exists("user://seen.txt"):
+			seen = FileAccess.open("user://seen.txt", FileAccess.READ).get_8()
+			DirAccess.remove_absolute("user://seen.txt")
+		if FileAccess.file_exists("user://high-score.txt"):
+			high_score = FileAccess.open("user://high-score.txt", FileAccess.READ).get_32()
+			DirAccess.remove_absolute("user://high-score.txt")
+		save_data()
+	
+	$HighScoreLabel.text = "High Score:\n" + str(high_score)
+	$AudioSlider.value = AudioServer.get_bus_volume_db(master_bus)
+	set_playtime()
+	set_evo()
+	
+	new_game()
+	#if FileAccess.file_exists("user://not-malware.exe"):
+		#FileAccess.open("user://not-malware.exe", FileAccess.WRITE)
+	
+
+func _process(_delta):
+	if playing:
+		if (
+				Input.is_action_just_pressed("drop") and $Player.visible
+				and get_local_mouse_position().distance_to($Container.position) < 600
+			):
+			$Player.visible = false
+			# later weight the rand so it makes more berrys or smth
+			# or not
+			$DropSound.play()
+			spawn_fruit(curr, Vector2(clamp(get_local_mouse_position().x, MIN_CLAMPS[curr], MAX_CLAMPS[curr]), 60), false)
+			curr = next
+			set_curr()
+			next = randi() % (game_seen + 1)
+		$Player.position = Vector2(clamp(get_local_mouse_position().x, MIN_CLAMPS[curr], MAX_CLAMPS[curr]), 60)
+		
+
+func spawn_fruit(index: int, pos: Vector2, poof: bool) -> void:
+	#await get_tree().create_timer(0.02).timeout
+	var fruit = load("res://fruit.tscn").instantiate()
+	add_child(fruit)
+	fruit.seeen.connect(on_seeen)
+	fruit.score.connect(add_score)
+	fruit.spawn_new.connect(spawn_fruit)
+	fruit.hit.connect(on_hit)
+	fruit.sett(index, pos)
+	if poof:
+		$Poof.position = pos
+		$Poof.scale = Vector2(POOF_SCALES[index], POOF_SCALES[index])
+		$Poof.visible = true
+		$Poof.play()
+		$MergeSound.play()
+		fruit.just_player_spawned = false
+	else:
+		await get_tree().create_timer(0.25).timeout
+		if is_instance_valid(fruit):
+			fruit.just_player_spawned = false
+		
+
+func on_hit() -> void:
+	if playing:
+		set_next()
+		$NextPoof.scale = Vector2(POOF_SCALES[next], POOF_SCALES[next])
+		$NextPoof.visible = true
+		$NextPoof.play()
+		if not $Player.visible:
+			$Player.visible = true
+	await get_tree().create_timer(0.5).timeout
+	check_death()
+	
+
+func on_seeen(index:int) -> void:
+	if seen < index:
+		seen = index
+		#save_data(index, high_score, playtime) # data is saved every sec
+		set_evo()
+	if game_seen < index and index < 5:
+		game_seen = index
+
+func add_score(s:int) -> void:
+	score += s
+	$ScoreLabel.text = "Score:\n" + str(score)
+	if score > high_score:
+		high_score = score
+		#save_data(seen, score, playtime) # data is saved every sec
+		$HighScoreLabel.text = "High Score:\n" + str(score)
+	
+
+func check_death() -> void:
+	for fruit in get_tree().get_nodes_in_group("fruit"):
+		if fruit.get_top_pos() < 90 and not fruit.just_player_spawned:
+			$Player.visible = false
+			$DeathLabel.visible = true
+			$RestartButton.visible = true
+			$DeathSound.play()
+			playing = false
+			
+
+func new_game() -> void:
+	for fruit in get_tree().get_nodes_in_group("fruit"):
+		fruit.queue_free()
+	game_seen = 0
+	curr = 0
+	next = 0
+	score = 0
+	set_curr()
+	set_next()
+	$ScoreLabel.text = "Score: 0"
+	$Player.visible = true
+	$DeathLabel.visible = false
+	$RestartButton.visible = false
+	playing = true
+
+func save_data():
+	var file = FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+	file.store_8(seen)
+	file.store_32(high_score)
+	file.store_32(playtime)
+	file.close()
+	
+
+# vv  sprite setters  vv
+func set_curr() -> void:
+	$Player/Cherry.visible = true
+	$Player/Strawberry.visible = true
+	$Player/Grape.visible = true
+	$Player/Tangerine.visible = true
+	$Player/Orange.visible = true
+	if curr != 0:
+		$Player/Cherry.visible = false
+	if curr != 1:
+		$Player/Strawberry.visible = false
+	if curr != 2:
+		$Player/Grape.visible = false
+	if curr != 3:
+		$Player/Tangerine.visible = false
+	if curr != 4:
+		$Player/Orange.visible = false
+	
+func set_next() -> void:
+	$Next/Cherry.visible = true
+	$Next/Strawberry.visible = true
+	$Next/Grape.visible = true
+	$Next/Tangerine.visible = true
+	$Next/Orange.visible = true
+	if next != 0:
+		$Next/Cherry.visible = false
+	if next != 1:
+		$Next/Strawberry.visible = false
+	if next != 2:
+		$Next/Grape.visible = false
+	if next != 3:
+		$Next/Tangerine.visible = false
+	if next != 4:
+		$Next/Orange.visible = false
+	
+func set_evo() -> void: # i for index
+	if seen >= 1:
+		$EvoWheel/FruitWheel/Strawberry.visible = true
+	if seen >= 2:
+		$EvoWheel/FruitWheel/Grape.visible = true
+	if seen >= 3:
+		$EvoWheel/FruitWheel/Tangerine.visible = true
+	if seen >= 4:
+		$EvoWheel/FruitWheel/Orange.visible = true
+	if seen >= 5:
+		$EvoWheel/FruitWheel/Apple.visible = true
+	if seen >= 6:
+		$EvoWheel/FruitWheel/Melon.visible = true
+	if seen >= 7:
+		$EvoWheel/FruitWheel/Peachnya.visible = true
+	if seen >= 8:
+		$EvoWheel/FruitWheel/Pineapple.visible = true
+	if seen >= 9:
+		$EvoWheel/FruitWheel/Wintermelon.visible = true
+	if seen >= 10:
+		$EvoWheel/FruitWheel/SUIKA.visible = true
+	
+func set_playtime() -> void:
+	var s: int = playtime % 60
+	var m: int = (playtime / 60) % 60
+	var hours: int = playtime / 3600
+	var seconds:= str(s)
+	var minutes:= str(m)
+	if len(str(s)) == 1:
+		seconds = "0" + str(s)
+	if len(str(m)) == 1:
+		minutes = "0" + str(m)
+	$PlaytimeLabel.text = "Playtime:\nH:MM:SS\n%s:%s:%s" % [hours, minutes, seconds]
+	
+# ^^  sprite setters  ^^
+
+func _on_playtime_timer_timeout():
+	playtime = playtime + 1
+	set_playtime()
+	save_data()
+	
+
+func _on_restart_pressed():
+	new_game()
+	
+func _on_bgm_finished():
+	$BGM.play()
+	
+
+func _on_poof_animation_finished():
+	$Poof.visible = false
+	
+
+func _on_next_poof_animation_finished():
+	$NextPoof.visible = false
+	
+
+func _on_credits_button_pressed():
+	$CreditsLabel.visible = not $CreditsLabel.visible
+	
+
+func _on_master_slider_value_changed(value):
+	AudioServer.set_bus_mute(master_bus, value < $AudioSlider.min_value + $AudioSlider.step)
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
